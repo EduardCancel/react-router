8f3f1b7afa192968ce7e127b2b3787b3fe687e97
IlTuoNome <laTuaEmail@example.com>
2025-04-01 16:58:40 +0200

    Update images in ListPost component for improved visuals and remove unused card

--- src/pages/ListPost.jsx
+++ src/pages/ListPost.jsx
@@ -7,7 +7,7 @@ export default function ListPost() {
                     <div className="card mb-3" >
                         <div className="row g-0">
                             <div className="col-md-4">
-                                <img src="https://picsum.photos/250/150?random" className="img-fluid rounded-start" />
+                                <img src="https://picsum.photos/400/200?grayscale" className="img-fluid rounded-start" />
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
@@ -21,7 +21,7 @@ export default function ListPost() {
                     <div className="card mb-3" >
                         <div className="row g-0">
                             <div className="col-md-4">
-                                <img src="https://picsum.photos/250/150?random" className="img-fluid rounded-start" />
+                                <img src="https://picsum.photos/400/200?grayscale" className="img-fluid rounded-start" />
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
@@ -35,7 +35,7 @@ export default function ListPost() {
                     <div className="card mb-3" >
                         <div className="row g-0">
                             <div className="col-md-4">
-                                <img src="https://picsum.photos/250/150?random" className="img-fluid rounded-start" />
+                                <img src="https://picsum.photos/400/200?grayscale" className="img-fluid rounded-start" />
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
@@ -49,7 +49,7 @@ export default function ListPost() {
                     <div className="card mb-3" >
                         <div className="row g-0">
                             <div className="col-md-4">
-                                <img src="https://picsum.photos/250/150?random" className="img-fluid rounded-start" />
+                                <img src="https://picsum.photos/400/200?grayscale" className="img-fluid rounded-start" />
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
@@ -63,7 +63,7 @@ export default function ListPost() {
                     <div className="card mb-3" >
                         <div className="row g-0">
                             <div className="col-md-4">
-                                <img src="https://picsum.photos/250/150?random" className="img-fluid rounded-start" />
+                                <img src="https://picsum.photos/400/200?grayscale" className="img-fluid rounded-start" />
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
@@ -74,21 +74,6 @@ export default function ListPost() {
                             </div>
                         </div>
                     </div>
-                    <div className="card mb-3 mt-3" >
-                        <div className="row g-0">
-                            <div className="col-md-4">
-                                <img src="https://picsum.photos/250/150?random" className="img-fluid rounded-start" />
-                            </div>
-                            <div className="col-md-8">
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
             </main >
         </>
