adad82dea2111c39f8071b154ea0fef2851a3ddb
IlTuoNome <laTuaEmail@example.com>
2025-04-01 16:40:01 +0200

    Refactor HomePage and ListPost components; add layout to Presentation component

--- src/pages/Presentation.jsx
+++ src/pages/Presentation.jsx
@@ -1,11 +1,29 @@
-import Header from "../components/Header"
 
 export default function Presentation() {
     return (
         <>
+            <main>
+                <div className="container text-center py-5">
+                    <div className="card" >
+                        <img src="https://picsum.photos/250/150?random" className="card-img-top" />
+                        <div className="card-body">
+                            <p className="card-text">
+                                Name : <span> Eduard - Cosmin</span>
+                            </p>
+                            <p className="card-text">
+                                Cognome : <span> Cancel</span>
+                            </p>
+                            <p className="card-text">
+                                Età :<span> 23 years</span>
+                            </p>
+                            <p className="card-text">
+                                Passioni : <span> Moto, Calcio, Coding</span>
+                            </p>
+                        </div>
+                    </div>
 
-
-
+                </div>
+            </main>
         </>
     )
 }
