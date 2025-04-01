f021b7926d9a4ee47cc6d452530f77354c3ba667
IlTuoNome <laTuaEmail@example.com>
2025-04-01 15:24:02 +0200

    Add routing and new components for HomePage, ListPost, and Presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ListPost from "./pages/ListPost";
+import Presentation from "./pages/Presentation";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
-
+        <Route path="/" Component={HomePage}>
+          <Route path="/listpost" Component={ListPost} />
+          <Route path="/presentation" Component={Presentation} />
         </Route>
       </Routes>
     </BrowserRouter>
